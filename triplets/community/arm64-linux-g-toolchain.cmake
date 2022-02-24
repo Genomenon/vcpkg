8889a4f5a00f023f50e395f5cd600fff17c73e2b
Clayton Wheeler <cwheeler@genomenon.com>
2022-02-24 17:03:06 -0600

    set CMAKE_SYSTEM_PROCESSOR

--- triplets/community/arm64-linux-g-toolchain.cmake
+++ triplets/community/arm64-linux-g-toolchain.cmake
@@ -2,6 +2,7 @@ set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
 #set(PKG_CONFIG_EXECUTABLE /usr/bin/aarch64-linux-gnu-pkg-config)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 set(CMAKE_C_COMPILER_TARGET aarch64-linux-gnu)
 set(CMAKE_CXX_COMPILER_TARGET aarch64-linux-gnu)
 set(CMAKE_ASM_COMPILER_TARGET aarch64-linux-gnu)
